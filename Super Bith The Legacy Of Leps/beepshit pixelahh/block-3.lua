451b84ae8ea1b64f452ea99683f3ba98e15cd062
sylvia <75647306+silkycell@users.noreply.github.com>
2025-02-07 16:50:50 -0500

    beepshi pixelahh

---
+++ Super Bith The Legacy Of Leps/beepshit pixelahh/block-3.lua
@@ -0,0 +1,27 @@
+local blockmanager = require("blockmanager")
+local sync = require("blocks/ai/synced")
+
+local blockID = BLOCK_ID
+
+local block = {}
+
+blockmanager.setBlockSettings({
+	id = blockID,
+	customhurt = true
+})
+
+function block.onCollideBlock(v,n)
+	if (n.__type == "Player") and Block.config[blockID].passthrough == false then
+		if n:mem(0x140, FIELD_WORD) == 0 and n:mem(0x13E, FIELD_WORD) == 0 then
+			n:harm()
+		end
+    end
+end
+
+function block.onInitAPI()
+    blockmanager.registerEvent(blockID, block, "onCollideBlock")
+end
+
+sync.registerBlinkingBlock(blockID, 1)
+
+return block
