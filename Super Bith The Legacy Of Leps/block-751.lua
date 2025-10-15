aa94010c44afe08ae10f1f9ca84a72580812ba4e
Tsu <82735980+tsuchi0@users.noreply.github.com>
2025-10-14 21:51:15 -0400

    ahh brother

---
+++ Super Bith The Legacy Of Leps/block-751.lua
@@ -0,0 +1,21 @@
+local blockmanager = require("blockmanager")
+
+local blockID = BLOCK_ID
+
+local block = {}
+
+blockmanager.setBlockSettings({
+	id = blockID,
+	bumpable = true
+})
+
+function block.onInitAPI()
+	registerEvent(block,"onPostBlockHit")
+end
+
+function block.onPostBlockHit(block, fromUpper, player)
+	if block.id ~= BLOCK_ID then return end
+	NPC.spawn(806, player.x, player.y, player.section)
+end
+
+return block
